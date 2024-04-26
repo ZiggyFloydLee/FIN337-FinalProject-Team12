0f9b7710b16130a0f6e4cdb5520de84b2026127e
ryant382 <rwt224@lehigh.edu>
2024-04-25 23:19:03 -0400

    Updates

--- app.py
+++ app.py
@@ -7,19 +7,25 @@ import zipfile
 # Define a function to load data
 @st.cache  # Use caching to load the data only once
 def load_data():
+    file_path = "inputs/practice_file.csv"
+    if os.path.exists(file_path):
+        data = pd.read_csv(file_path)
+        return data
+    else:
+        raise FileNotFoundError(f"The file {file_path} does not exist.")
+#def load_data():
     # Define the path to the .dta file
-    ccm_youngfirms = "inputs/ccm_youngfirms_2000_2018.dta/ccm_youngfirms_2000_2018.dta"
+    #ccm_youngfirms = pd.read_csv("inputs/practice_file.csv")
 
-    # Check if the .dta file exists, if not, unzip it
-    if not os.path.exists(ccm_youngfirms):
-        zip_path = "inputs/ccm_youngfirms_2000_2018.zip"
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall("inputs/ccm_youngfirms_2000_2018.dta")
+    # # Check if the .dta file exists, if not, unzip it
+    # if not os.path.exists(ccm_youngfirms):
+    #     zip_path = "inputs/ccm_youngfirms_2000_2018.zip"
+    #     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    #         zip_ref.extractall("inputs/ccm_youngfirms_2000_2018.dta")
 
-    # Load the data from the .dta file
-    data = pd.read_stata(ccm_youngfirms)
-    return data
-
+    # # Load the data from the .dta file
+    # data = pd.read_stata(ccm_youngfirms)
+    # return data
 data = load_data()
 
 # Dashboard setup
@@ -33,7 +39,7 @@ col1, col2 = st.columns((1, 2))
 with col1:
     st.subheader("User Inputs")
     year = st.number_input('Year', min_value=2000, max_value=2018, value=2018)
-    industry_sector = st.selectbox('Industry Sector', options=data['naicsh'].unique())
+    industry_sector = st.selectbox('Industry Sector', options=data['Industry'].unique())
     
 
 # Plots on the right column
